d9ab0d89e405944b8998996a4c8e88f34fc5314a
Jaewon Noh <opqw987@gmail.com>
2024-03-21 16:01:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 21.19 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(seoul: Array<String>): String {
+        var answer = ""
+        var index = seoul.indexOf("Kim")
+        
+        answer = "김서방은 ${index}에 있다"
+        
+        return answer
+    }
+}
