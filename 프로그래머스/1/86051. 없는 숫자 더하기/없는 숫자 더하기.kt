f5413a7f7084f760209fc38fc6e349ad3c26219e
Jaewon Noh <opqw987@gmail.com>
2024-03-25 15:08:47 +0900

    답안 추가

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.kt
@@ -9,3 +9,11 @@ class Solution {
         return answer
     }
 }
+
+class Solution2 {
+    fun solution(numbers: IntArray): Int = (0..9).filterNot(numbers::contains).sum()
+}
+
+class Solution3 {
+    fun solution(numbers: IntArray): Int  = 45 - numbers.sum()
+}
