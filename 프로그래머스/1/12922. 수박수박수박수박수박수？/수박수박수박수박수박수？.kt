e762a1170953aad1d20323b9e9060e8b131e80a5
Jaewon Noh <opqw987@gmail.com>
2024-03-28 14:21:04 +0900

    답안 추가

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.kt
@@ -1,3 +1,12 @@
 class Solution {
     fun solution(n: Int): String = "수박".repeat (n / 2) + if (n % 2 == 1) "수" else ""
+
+    fun solution2(n: Int): String {
+        var answer = ""
+        var keyword = "수박씨앗"
+
+        answer = keyword.repeat (n / keyword.length) + keyword.substring(0, n % keyword.length)
+
+        return answer
+    }
 }
