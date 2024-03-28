9ea840469a49b7fe79ab87aa57001a1beb7830b1
Jaewon Noh <opqw987@gmail.com>
2024-03-28 14:12:33 +0900

    답안 추가

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.kt
@@ -9,3 +9,10 @@ class Solution {
         }
     }
 }
+
+class Solution2 {
+    fun solution(s: String) =
+    // with로 s 문자열을 수신 객체로 지정해 람다 안에서 객체를 추가로 명시하지 않아 간결함
+    // substring 을 index가 아닌 IntRange를 사용해서 length의 홀/짝을 구분하지 않음
+      with(s) { substring(length / 2 - 1 + (length % 2) .. length / 2) }
+  }
