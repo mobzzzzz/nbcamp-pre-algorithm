74c5c65d015008bd0d84510db04bf7feef900144
Jaewon Noh <opqw987@gmail.com>
2024-04-01 14:21:10 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 8.77 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.kt
@@ -1,24 +1,3 @@
 class Solution {
-    fun solution(left: Int, right: Int): Int {
-        var answer: Int = 0
-        
-        for (i in left..right) {
-            answer += if (i.divisorCount() % 2 == 1) i * -1 else i
-        }
-        
-        return answer
-    }
-    
-    fun Int.divisorCount(): Int {
-        var count = 0
-        val sqrt = Math.sqrt(this.toDouble()).toInt()
-        
-        for (i in 1..sqrt) {
-            if (this % i == 0) {
-                count += if (i * i == this) 1 else 2
-            }
-        }
-        
-        return count
-    }
+    fun solution(left: Int, right: Int): Int = (left..right).map { i -> if ((1..i).filter { i % it == 0 }.size % 2 == 0) i else -i }.sum()
 }
