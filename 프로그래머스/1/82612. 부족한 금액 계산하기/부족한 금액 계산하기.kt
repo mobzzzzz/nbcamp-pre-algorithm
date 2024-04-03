cb93d207a9e0a7c4f3d3e6cfc6ab65f38f527daf
Jaewon Noh <opqw987@gmail.com>
2024-04-03 15:00:23 +0900

    답안 추가

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.kt
@@ -1,3 +1,7 @@
 class Solution {
-    fun solution(price: Int, money: Int, count: Int): Long = ((1..count).fold(0L) { total, num -> price.toLong() * num + total } - money).coerceAtLeast(0)
+    fun solution(price: Int, money: Int, count: Int): Long = ((1..count).sumOf { price.toLong() * it } - money).coerceAtLeast(0)
+
+    fun solution2(price: Int, money: Int, count: Int): Long = ((1..count).fold(0L) { total, num -> price.toLong() * num + total } - money).coerceAtLeast(0)
+
+    fun solution3(price: Int, money: Int, count: Int): Long = (1..count).sumOf { price.toLong() * it }.let { if (money > it) 0 else it - money }
 }
