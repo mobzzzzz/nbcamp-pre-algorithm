d0d0b8f6c1e8e97c33a69358e96e3c657d61950b
Jaewon Noh <opqw987@gmail.com>
2024-04-04 15:35:12 +0900

    답안 추가

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.kt
@@ -1,3 +1,11 @@
 class Solution {
     fun solution(s: String): Boolean = s.matches("\\d{4}|\\d{6}".toRegex())
+
+    fun solution2(s: String): Boolean = s.matches("[0-9]{4}|[0-9]{6}".toRegex())
+
+    fun solution3(s: String): Boolean = s.matches("\\d{4}(?:\\d{2})?".toRegex())
+
+    fun solution4(s: String) = (s.length == 4 || s.length == 6) && s.toIntOrNull() != null
+
+    fun solution5(s: String) = s.all { it.isDigit() } && ((s.length == 4) || (s.length == 6))
 }
