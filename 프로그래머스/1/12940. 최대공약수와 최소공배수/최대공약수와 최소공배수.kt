d25b214cbb0526ef82594f00ca5fbbafa5ac3953
Jaewon Noh <opqw987@gmail.com>
2024-04-09 15:10:32 +0900

    답안 추가

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.kt
@@ -7,4 +7,12 @@ class Solution {
             else -> chda(m, n % m)
         }
     }
+
+    fun solution2(n: Int, m: Int): IntArray {
+        val gcd = gcd(n, m)
+
+        return intArrayOf(gcd, n * m / gcd)
+    }
+
+    tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
