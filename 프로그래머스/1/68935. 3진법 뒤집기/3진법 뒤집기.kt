64f51d8e2d6a5878aec91cf491f71cbf22cfc117
Jaewon Noh <opqw987@gmail.com>
2024-04-11 14:38:07 +0900

    답안 추가

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.kt
@@ -6,11 +6,16 @@ class Solution {
         
         while (number > 0) {
             list.add(number % 3)
+            list.add(0, number % 3)
             number /= 3
         }
         
         answer = list.foldIndexed(0) { index, acc, num -> acc + (num * Math.pow(3.0, list.count() - index - 1.0)).toInt() }
         
         return answer
+    }
+
+    fun solution2(n: Int): Int {
+        return n.toString(3).reversed().toInt(3)
     }
 }
