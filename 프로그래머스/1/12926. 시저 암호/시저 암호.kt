835d884042cd8d0faed2b511d73ad616becb449c
Jaewon Noh <opqw987@gmail.com>
2024-04-24 09:43:36 +0900

    시저 암호 답안 추가

--- 프로그래머스/1/12926. 시저 암호/시저 암호.kt
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.kt
@@ -8,4 +8,22 @@ class Solution {
                 }
         }
     }.joinToString("")
+    
+    fun solution2(s: String, n: Int): String =
+        s.toList().joinToString(separator = "") {
+            when (it) {
+                in 'A'..'Z' -> ('A'.toInt() + (it.toInt() - 'A'.toInt() + n) % ('Z' - 'A' + 1)).toChar()
+                in 'a'..'z' -> ('a'.toInt() + (it.toInt() - 'a'.toInt() + n) % ('z' - 'a' + 1)).toChar()
+                else -> it
+            }.toString()
+        }
+
+        fun solution3(s: String, n: Int): String =
+        s.map {
+            when {
+                it.isLowerCase() -> 'a' + (it + n - 'a') % 26
+                it.isUpperCase() -> 'A' + (it + n - 'A') % 26
+                else -> ' '
+            }
+        }.joinToString("")
 }
