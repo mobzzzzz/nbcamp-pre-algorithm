7c16b47ddd5c842d313a38f55fcd4245312fd843
Jaewon Noh <opqw987@gmail.com>
2024-04-25 09:28:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 6.90 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.kt
@@ -1,21 +1,14 @@
 class Solution {
-    fun solution(s: String): Int {
-        var result = s
-        val numberPatternMap = mapOf(
-            "zero" to 0,
-            "one" to 1,
-            "two" to 2,
-            "three" to 3,
-            "four" to 4,
-            "five" to 5,
-            "six" to 6,
-            "seven" to 7,
-            "eight" to 8,
-            "nine" to 9
-        )
-        
-        numberPatternMap.forEach { result = result.replace(it.key, it.value.toString()) }
-        
-        return result.toInt()
-    }
+    fun solution(s: String): Int = s
+    .replace("one", "1")
+    .replace("two", "2")
+    .replace("three", "3")
+    .replace("four", "4")
+    .replace("five", "5")
+    .replace("six", "6")
+    .replace("seven", "7")
+    .replace("eight", "8")
+    .replace("nine", "9")
+    .replace("zero", "0")
+    .toInt()
 }
