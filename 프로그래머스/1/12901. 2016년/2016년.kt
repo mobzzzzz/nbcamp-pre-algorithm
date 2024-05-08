fe68914d09d46c98d227012b701c4cc4e247cb48
Jaewon Noh <opqw987@gmail.com>
2024-05-08 16:43:08 +0900

    Update 2016년.kt

--- 프로그래머스/1/12901. 2016년/2016년.kt
+++ 프로그래머스/1/12901. 2016년/2016년.kt
@@ -4,4 +4,22 @@ import java.util.Locale
 
 class Solution {
     fun solution(a: Int, b: Int): String = LocalDate.of(2016, a, b).getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.US).toUpperCase()
+
+    fun solution(a: Int, b: Int): String {
+        val week = listOf("THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED")
+
+        var answer = ""
+        var days = b
+
+        for (i in 1 until a) {
+            days += when (i) {
+                1, 3, 5, 7, 8, 10, 12 -> 31
+                2 -> 29
+                else -> 30
+            }
+        }
+
+        answer = week[days % 7]
+        return answer
+    }
 }
