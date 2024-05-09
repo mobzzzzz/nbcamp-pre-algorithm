fd4199ac50cd77c90e8861283ca9e5e97dbbfe00
Jaewon Noh <opqw987@gmail.com>
2024-05-09 10:44:03 +0900

    Update 카드 뭉치.kt

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.kt
@@ -23,5 +23,16 @@ class Solution {
     answer = if (breakFlag) "No" else "Yes"
 
     return answer
-}
+    }
+
+    fun solution2(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        var idx1 = 0
+        var idx2 = 0
+        goal.forEach {
+            if (idx1 < cards1.size && it == cards1[idx1]) idx1++
+            else if (idx2 < cards2.size && it == cards2[idx2]) idx2++
+            else return "No"
+        }
+        return "Yes"
+    }
 }
