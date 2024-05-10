7d05614a9616c019f5208499a12f3dcfe3e654bf
Jaewon Noh <opqw987@gmail.com>
2024-05-10 09:55:42 +0900

    Update 과일 장수.kt

--- 프로그래머스/1/135808. 과일 장수/과일 장수.kt
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.kt
@@ -5,4 +5,25 @@ class Solution {
             .map {
                 it.minOrNull()!! * it.count()
             }.sum()
+
+    fun solution2(k: Int, m: Int, score: IntArray): Int {
+        var answer: Int = 0
+        val apple =  Array<Int>(k) {0}
+    
+        for(s in score) {
+            apple[s-1] += 1
+        }
+    
+        for(i in k-1 downTo 0) {
+           if(apple[i] % m != 0 && i > 0) {
+               val down = apple[i] % m
+               apple[i] -= down
+               apple[i-1] += down
+           }
+           answer += (apple[i] / m) * (i+1) * m
+        }
+    
+    
+        return answer
+    }
 }
