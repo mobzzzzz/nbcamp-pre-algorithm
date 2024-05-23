7567d6b9c6a837ddbab3406ce5c88860293d2df1
Jaewon Noh <opqw987@gmail.com>
2024-05-23 09:21:16 +0900

    Update 숫자 짝꿍.kt

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.kt
@@ -29,4 +29,51 @@ class Solution {
         else if (pairText.all { it == '0' }) return "0"
         return pairText
     }
+
+    fun solution3(X: String, Y: String): String {
+        val pairNumList = mutableListOf<Char>()
+        var xList = X.toMutableList()
+        var yList = Y.toMutableList()
+        var index = 0
+
+        while (xList.isNotEmpty()) {
+            val xChar = xList[index]
+
+            yList.firstOrNull { it == xChar }?.let {
+                xList.removeAt(xList.indexOf(xChar))
+                yList.removeAt(yList.indexOf(xChar))
+                pairNumList.add(xChar)
+            } ?: xList.removeAll { it == xChar }
+        }
+
+        if (pairNumList.isEmpty()) return "-1"
+        else if (pairNumList.all { it == '0' }) return "0"
+        return pairNumList.sortedDescending().joinToString("")
+    }
+
+    fun solution2(X: String, Y: String): String {
+        val xMap = mutableMapOf<Char, Int>()
+        val yMap = mutableMapOf<Char, Int>()
+        var pairText = ""
+
+        X.forEach {
+            xMap[it] = xMap.getOrDefault(it, 0) + 1
+        }
+
+        Y.forEach {
+            yMap[it] = yMap.getOrDefault(it, 0) + 1
+        }
+
+        (9 downTo 0).forEach { currentNum ->
+            val xCount = xMap.getOrDefault(currentNum.digitToChar(), 0)
+            val yCount = yMap.getOrDefault(currentNum.digitToChar(), 0)
+            val minCount = minOf(xCount, yCount)
+
+            repeat(minCount) { pairText += currentNum }
+        }
+
+        if (pairText.isEmpty()) return "-1"
+        else if (pairText.all { it == '0' }) return "0"
+        return pairText
+    }
 }
