071b6bfb85af2393c32f234f0e13dd489c350860
Jaewon Noh <opqw987@gmail.com>
2024-05-31 15:39:00 +0900

    Update 햄버거 만들기.kt

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.kt
@@ -15,4 +15,30 @@ class Solution {
         
         return answer
     }
+
+    fun solution2(ingredient: IntArray): Int {
+        var answer: Int = 0
+
+        var stack = Stack<Int>()
+
+        ingredient.map { value ->
+            stack.add(value)
+
+            if (stack.size >= 4 && stack.peek() == 1) {
+                val first = stack[stack.size - 4] == 1
+                val second = stack[stack.size - 3] == 2
+                val third = stack[stack.size - 2] == 3
+                val fourth = stack[stack.size - 1] == 1
+
+                if (first && second && third && fourth) {
+                    repeat(4) { stack.pop() }
+                    answer ++
+                }
+
+            }
+
+        }
+
+        return answer
+    }
 }
