ccb0f789daee758e67025aeb468a3be05f8a3cb4
Jaewon Noh <opqw987@gmail.com>
2024-06-20 15:42:15 +0900

    Update 신고 결과 받기.kt

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.kt
@@ -27,4 +27,16 @@ class Solution {
 
         return answer.values.toIntArray()
     }
+
+    fun solution2(id_list: Array<String>, report: Array<String>, k: Int): IntArray =
+    report.map { it.split(" ") }
+        .groupBy { it[1] }
+        .asSequence()
+        .map { it.value.distinct() }
+        .filter { it.size >= k }
+        .flatten()
+        .map { it[0] }
+        .groupingBy { it }
+        .eachCount()
+        .run { id_list.map { getOrDefault(it, 0) }.toIntArray() }
 }
