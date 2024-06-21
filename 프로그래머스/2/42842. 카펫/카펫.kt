626816895ceb2a61fd293b93c08505cc79cdd110
Jaewon Noh <opqw987@gmail.com>
2024-06-21 09:54:31 +0900

    Update 카펫.kt

--- 프로그래머스/2/42842. 카펫/카펫.kt
+++ 프로그래머스/2/42842. 카펫/카펫.kt
@@ -22,4 +22,11 @@ class Solution {
 
         return answer
     }
+
+    fun solution2(brown: Int, red: Int): IntArray {
+        return (1..red)
+            .filter { red % it == 0 }
+            .first { brown == (red / it * 2) + (it * 2) + 4 }
+            .let { intArrayOf(red / it + 2, it + 2) }
+    }
 }
